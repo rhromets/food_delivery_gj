d0db07f86ad6b29ec84b47d49bf2d47065a86b0c
Roman Khromets <rhromets@gmail.com>
2024-07-21 12:06:08 +0300

    feat: add an empty state of cart history page

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:food_delivery_gj/base/no_data_page.dart';
 import 'package:food_delivery_gj/controllers/cart_controller.dart';
 import 'package:food_delivery_gj/models/cart_model.dart';
 import 'package:food_delivery_gj/routes/route_helper.dart';
@@ -62,99 +63,118 @@ class CartHistory extends StatelessWidget {
               ],
             ),
           ),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(
-                top: Dimensions.height20,
-                left: Dimensions.width20,
-                right: Dimensions.width20,
-              ),
-              child: MediaQuery.removePadding(
-                removeTop: true,
-                context: context,
-                child: ListView(
-                  children: [
-                    for (int i = 0; i < itemsPerOrder.length; i++)
-                      Container(
-                        height: Dimensions.height45 * 3,
-                        margin: EdgeInsets.only(bottom: Dimensions.height20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            (() {
-                              DateTime parseDate =
-                                  DateFormat('yyyy-MM-dd HH:mm:ss').parse(
-                                      getCartHistoryList[listCounter].time!);
-                              var inputDate =
-                                  DateTime.parse(parseDate.toString());
-                              var outputFormat =
-                                  DateFormat('dd/MM/yyyy hh:mm a');
-                              var outputDate = outputFormat.format(inputDate);
-                              return BigText(text: outputDate);
-                            }()),
-                            SizedBox(height: Dimensions.height10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          GetBuilder<CartController>(builder: (cartController) {
+            return cartController.getCartHistoryList().isNotEmpty
+                ? _getCartHistoryPage(context, itemsPerOrder,
+                    getCartHistoryList, listCounter, cartOrderTimeToList)
+                : _getEmptyPage(context);
+          })
+        ],
+      ),
+    );
+  }
+
+  SizedBox _getEmptyPage(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 1.5,
+      child: const Center(
+        child: NoDataPage(
+          text: 'You didn\'t buy anything so far !',
+          imgPath: 'assets/images/empty_box.png',
+        ),
+      ),
+    );
+  }
+
+  Expanded _getCartHistoryPage(
+      BuildContext context,
+      List<int> itemsPerOrder,
+      List<CartModel> getCartHistoryList,
+      int listCounter,
+      List<String> Function() cartOrderTimeToList) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: Dimensions.height20,
+          left: Dimensions.width20,
+          right: Dimensions.width20,
+        ),
+        child: MediaQuery.removePadding(
+          removeTop: true,
+          context: context,
+          child: ListView(
+            children: [
+              for (int i = 0; i < itemsPerOrder.length; i++)
+                Container(
+                  height: Dimensions.height45 * 3,
+                  margin: EdgeInsets.only(bottom: Dimensions.height20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      (() {
+                        DateTime parseDate = DateFormat('yyyy-MM-dd HH:mm:ss')
+                            .parse(getCartHistoryList[listCounter].time!);
+                        var inputDate = DateTime.parse(parseDate.toString());
+                        var outputFormat = DateFormat('dd/MM/yyyy hh:mm a');
+                        var outputDate = outputFormat.format(inputDate);
+                        return BigText(text: outputDate);
+                      }()),
+                      SizedBox(height: Dimensions.height10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Wrap(
+                            direction: Axis.horizontal,
+                            children: List.generate(itemsPerOrder[i], (index) {
+                              if (listCounter < getCartHistoryList.length) {
+                                listCounter++;
+                              }
+                              return index <= 2
+                                  ? Container(
+                                      height: Dimensions.height40 * 2,
+                                      width: Dimensions.width20 * 4,
+                                      margin: EdgeInsets.only(
+                                          right: Dimensions.width10 / 2),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                            Dimensions.radius15 / 2),
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                              "${AppConstants.BASE_URL}${AppConstants.UPLOAD_URL}${getCartHistoryList[listCounter - 1].img}"),
+                                        ),
+                                      ),
+                                    )
+                                  : Container();
+                            }),
+                          ),
+                          SizedBox(
+                            height: Dimensions.height30 * 3,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
-                                Wrap(
-                                  direction: Axis.horizontal,
-                                  children:
-                                      List.generate(itemsPerOrder[i], (index) {
-                                    if (listCounter <
-                                        getCartHistoryList.length) {
-                                      listCounter++;
-                                    }
-                                    return index <= 2
-                                        ? Container(
-                                            height: Dimensions.height40 * 2,
-                                            width: Dimensions.width20 * 4,
-                                            margin: EdgeInsets.only(
-                                                right: Dimensions.width10 / 2),
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(
-                                                      Dimensions.radius15 / 2),
-                                              image: DecorationImage(
-                                                fit: BoxFit.cover,
-                                                image: NetworkImage(
-                                                    "${AppConstants.BASE_URL}${AppConstants.UPLOAD_URL}${getCartHistoryList[listCounter - 1].img}"),
-                                              ),
-                                            ),
-                                          )
-                                        : Container();
-                                  }),
+                                const SmallText(
+                                  text: 'Total',
+                                  color: AppColors.titleColor,
                                 ),
-                                SizedBox(
-                                  height: Dimensions.height30 * 3,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      const SmallText(
-                                        text: 'Total',
-                                        color: AppColors.titleColor,
-                                      ),
-                                      BigText(
-                                        text: '${itemsPerOrder[i]} Items',
-                                        color: AppColors.titleColor,
-                                      ),
-                                      _orderAgainButton(cartOrderTimeToList,
-                                          getCartHistoryList, i),
-                                    ],
-                                  ),
+                                BigText(
+                                  text: '${itemsPerOrder[i]} Items',
+                                  color: AppColors.titleColor,
                                 ),
+                                _orderAgainButton(
+                                    cartOrderTimeToList, getCartHistoryList, i),
                               ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          )
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
